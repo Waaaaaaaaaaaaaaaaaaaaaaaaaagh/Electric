38a4b762e4dac5cecbd23c38975bb0f2e22268a6
wdl <2230771855@qq.com>
2022-07-30 01:47:48 +0800

    dhuidh

--- App/src/main.c
+++ App/src/main.c
@@ -37,30 +37,30 @@ int main()
 
 	while(1)
 	{
-		// if(ADC_ConvertedValue1_flag||ADC_ConvertedValue2_flag)
-		// {
-		// 	DMA_Cmd(ADC_DMA_CHANNEL,ENABLE);
-		// 	ADC_Cmd(ADC_x, ENABLE);
-    	// 	// 初始化ADC 校准寄存器
-    	// 	ADC_ResetCalibration(ADC_x);
-    	// 	// 等待校准寄存器初始化完成
-    	// 	while (ADC_GetResetCalibrationStatus(ADC_x));
-    	// 	// ADC开始校准
-    	// 	ADC_StartCalibration(ADC_x);
-    	// 	// 等待校准完成
-    	// 	while (ADC_GetCalibrationStatus(ADC_x));
-    	// 	// 由于没有采用外部触发，所以使用软件触发ADC转换
-    	// 	ADC_SoftwareStartConvCmd(ADC_x, ENABLE);
-		// 	if(ADC_ConvertedValue1_flag==1)
-		// 	{
-		// 		ADC_ConvertedValue1_flag=0;
-		// 		for_max_time(ADC_ConvertedValue1);
-		// 	}
-		// 	else 
-		// 	{
-		// 		ADC_ConvertedValue2_flag=0;
-		// 		for_max_time(ADC_ConvertedValue2);
-		// 	}
-		// }
+		if(ADC_ConvertedValue1_flag||ADC_ConvertedValue2_flag)
+		{
+			DMA_Cmd(ADC_DMA_CHANNEL,ENABLE);
+			ADC_Cmd(ADC_x, ENABLE);
+    		// 初始化ADC 校准寄存器
+    		ADC_ResetCalibration(ADC_x);
+    		// 等待校准寄存器初始化完成
+    		while (ADC_GetResetCalibrationStatus(ADC_x));
+    		// ADC开始校准
+    		ADC_StartCalibration(ADC_x);
+    		// 等待校准完成
+    		while (ADC_GetCalibrationStatus(ADC_x));
+    		// 由于没有采用外部触发，所以使用软件触发ADC转换
+    		ADC_SoftwareStartConvCmd(ADC_x, ENABLE);
+			if(ADC_ConvertedValue1_flag==1)
+			{
+				ADC_ConvertedValue1_flag=0;
+				for_max_time(ADC_ConvertedValue1);
+			}
+			else 
+			{
+				ADC_ConvertedValue2_flag=0;
+				for_max_time(ADC_ConvertedValue2);
+			}
+		}
 	}
 }
